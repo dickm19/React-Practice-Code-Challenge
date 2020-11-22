09a539282eae35d8c8bd9138408d72e7e31db3b5
dickm19 <maryclairedick@gmail.com>
2020-11-22 18:00:35 -0500

    not very far

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,18 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushiList: []
+  }
+
+  componentDidMount(){
+    return fetch(API).then(resp=>resp.json()).then(sushis => this.setState({sushiList: sushis}))
+  }
+  
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer sushis={this.state.sushiList} />
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,31 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+class SushiContainer extends React.Component {
+
+  state = {
+    counter: 0,
+    clicked: false
+  }
+
+
+  renderSushis = () => {
+    return this.props.sushis.map(sushiObj => <Sushi key={sushiObj.id} sushi={sushiObj}/>)
+  }
+
+  render(){
+    
+    return (
+      <Fragment>
+        <div className="belt">
+          <MoreButton />
+        </div>
+
+      </Fragment>
+    )
+
+  }
 }
 
 export default SushiContainer
